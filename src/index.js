e3bcdc45cca5634699bee5ed9c6577531711dfca
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-04-08 13:48:18 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    handleToDo(e.target.new-task-description.value);
+    form.reset();
+  })
 });
+
+function handleToDo(input) {
+  let li = document.createElement("li");
+  let button = document.createElement("button");
+  button.addEventListener("click", function (e) {
+    e.target.parentNode.remove();
+  })
+  button.textContent = "x";
+  li.textContent = `${input} `;
+  li.appendChild(button);
+  document.querySelector("#tasks").appendChild(li);
+}
